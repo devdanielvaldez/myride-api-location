971ba53d933c6fff52c15fe3f02d7680ba1d5bd6
Daniel Valdez <hello@daniel-valdez.com>
2024-07-10 21:59:07 -0400

    add features

--- server.js
+++ server.js
@@ -26,6 +26,27 @@ const tripSchema = new mongoose.Schema({
 
 const Trip = mongoose.model('Trip', tripSchema);
 
+const busSchema = new mongoose.Schema({
+  busId: String,
+  name: String,
+  pointA: String,
+  pointB: String,
+  pointALat: String,
+  pointBLat: String,
+  pointALong: String,
+  pointBLong: String,
+  horaSalida: String,
+  locations: [
+    {
+      latitude: Number,
+      longitude: Number,
+      timestamp: { type: Date, default: Date.now },
+    },
+  ],
+});
+
+const Bus = mongoose.model('Bus', busSchema);
+
 // Middleware para parsear el cuerpo de las peticiones
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -41,6 +62,55 @@ app.post('/trips', async (req, res) => {
   await newTrip.save();
   res.status(201).send(newTrip);
 });
+
+app.post('/buses', async (req, res) => {
+  try {
+    const busData = req.body;
+    const newBus = new Bus(busData);
+    await newBus.save();
+    res.status(201).json(newBus);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.get('/buses', async (req, res) => {
+  try {
+    const findBus = await Bus.find();
+    res.status(200).json(findBus);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.get('/buses/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const findBus = await Bus.findById(id);
+    res.status(200).json(findBus.locations[0]);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.put('/buses/:id', async (req, res) => {
+  const { id } = req.params;
+  const { latitude, longitude } = req.body;
+  const buses = await Bus.findById(id);
+  if (buses) {
+    if (buses.locations.length > 0) {
+      buses.locations[buses.locations.length - 1] = { latitude, longitude, timestamp: new Date() };
+    } else {
+      buses.locations.push({ latitude, longitude, timestamp: new Date() });
+    }
+    await buses.save();
+    res.status(200).send(buses);
+  } else {
+    res.status(404).send('Trip not found');
+  }
+});
+
+
 
 // Endpoint para actualizar la ubicación de un viaje
 app.put('/trips/:tripId/locations', async (req, res) => {
